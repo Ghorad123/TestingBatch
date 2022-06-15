c58ebe1b9939ebeb482c4ac41b94243dff58018b
Rahul <Rahul@Rahul>
2022-06-15 23:20:04 +0530

    by rahul

--- 15 June GITTB15/src/com/cjc/Demo3.java
+++ 15 June GITTB15/src/com/cjc/Demo3.java
@@ -2,9 +2,13 @@ package com.cjc;
 
 public class Demo3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+         System.out.println("new class");
+	}
+	public void m1()
+	{
+		System.out.println("IN M1");
 	}
 
 }
