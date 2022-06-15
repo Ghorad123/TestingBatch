60216f65c73eef9948f07282c4e7ec8f60574d19
Ghorad123 <107461894+Ghorad123@users.noreply.github.com>
2022-06-15 11:57:51 +0530

    Update GitDemo.java

--- 15 June GITTB15/src/com/cjc/GitDemo.java
+++ 15 June GITTB15/src/com/cjc/GitDemo.java
@@ -19,12 +19,13 @@ public class GitDemo {
 	{
 		System.out.println("In m4 method")
 	}
+	public void m5()
+	{
+		System.out.println("In m5 method")
+	}
 	public static void main(String[] args) 
 	{
       System.out.println("Hello Selenium");
-      
-      
-      
-	}
+        }
 
 }
