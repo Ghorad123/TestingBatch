bd50b41818009b9a047813fd4c8906265c553cd7
Rahul <Rahul@Rahul>
2022-06-15 11:11:41 +0530

    by cjc

--- 15 June GITTB15/src/com/cjc/GitDemo.java
+++ 15 June GITTB15/src/com/cjc/GitDemo.java
@@ -2,9 +2,15 @@ package com.cjc;
 
 public class GitDemo {
 
+	public void m1()
+    {
+  	  System.out.println("In m1 method");
+    }
 	public static void main(String[] args) 
 	{
       System.out.println("Hello Selenium");
+      
+      
       
 	}
 
